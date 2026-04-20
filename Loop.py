153c2f0edc1ac26f78b9b3ebdb43b85cf1b0cd6b
Elmurod04 <elmurod3219@gmail.com>
2026-04-20 14:49:12 +0500

    first commit

---
+++ Loop.py
@@ -0,0 +1,6 @@
+fruits = ["Apple", "Apricot", "Avocado", "Acai", "Ackee", "Amla", "Almond"]
+afruits = fruits.copy()
+
+afruits.append("Acerola")
+print(fruits)
+print(afruits)
---
+++ Python lists.py
@@ -0,0 +1,7 @@
+high_income = True
+good_credit = False
+
+if high_income and good_credit:
+    print("Eligible")
+else:
+    print("Not Eligible")
