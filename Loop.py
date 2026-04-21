c9c60604381151c1ee626680b3983ccf1bc37e24
Elmurod04 <elmurod3219@gmail.com>
2026-04-21 17:18:10 +0500

    Python dictionaries

--- Loop.py
+++ Loop.py
@@ -1,7 +1,16 @@
-mytuple = ("nose", "ear", "eye", "mouth", "chin")
-mytuple = mytuple + ("forehead",)
-print(len(mytuple))
-if "nose" in mytuple:
-    print("Yes")
-else:
-    print("No")
+# Create the dictionary
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": "2024"
+}
+# Print the model
+print(car["model"])
+# Add a color key
+car["color"] = "red"
+# Remove the brand key using pop
+car.pop("brand")
+
+# Print the dictionary
+
+print(car)
